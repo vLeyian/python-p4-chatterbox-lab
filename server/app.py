553129404b499a81e24171c55d2f073e45ee39b8
Leyian <victor.leyian@student.moringaschool.com>
2024-04-10 12:08:32 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,53 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    print(request.method)
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        return make_response(messages, 200)
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get("body"),
+            username=request.json.get("username")
+        )
+        print(new_message)
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    
+    if message == None:
+        response_body = {
+            "message": "This record does not exist in our database. Please try again."
+        }
+        return make_response(response_body, 404)
+        
+    else:
+        if request.method == 'GET':
+            return make_response(message.to_dict(), 200)
+            
+        elif request.method == 'PATCH':
+            for attr in request.json:
+                setattr(message, attr, request.json.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            return make_response(message.to_dict(), 200)
+            
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+            
+            response_body = {
+                "delete_successful": True,
+                "message": "Message deleted."
+            }
+            
+            return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
